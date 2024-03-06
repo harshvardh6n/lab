c3b5f75be52a827e3b0922ed82223a1bd7f539e6
Harshvardhan Singh <111485010+harshvardh6n@users.noreply.github.com>
2024-03-06 13:12:49 +0530

    jan10

---
+++ jan10/message.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("If The End Is Good,\n");
+	printf("Then It Is Good,\n");
+	printf("Whatever Be The Means.\n");
+	return 0;
+}
---
+++ jan10/message2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("If The End Is Good,\n Then It Is Good,\n Whatever Be The Means.\n");
+	return 0;
+}
